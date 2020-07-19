d008e1ee5da5051650cd5e7810df5793664a7845
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-07-19 02:03:52 -0300

    updateRoute

--- Framework/engine.js
+++ Framework/engine.js
@@ -2,4 +2,15 @@ class EngineApp{
     render(object){
         console.log(object)
     }
+    gotoLink(object,link){
+        document.getElementById(object).addEventListener("click",this.executionLink,false)
+    }
+    executionLink(){
+        window.location.href="http://127.0.0.1:5500/project/index.html#pages/02"
+        location.reload()
+    }
+}
+
+window.onload = function(){
+    console.log(window.location.href)
 }
