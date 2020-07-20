31e74ade4c61cb3adac5d77f25b77308c323a345
Felipe Catão do Nascimento <34559081+FelipeKatao@users.noreply.github.com>
2020-07-20 00:56:04 -0300

    Revert "modify routes dont find"

--- Framework/routesEngine.js
+++ Framework/routesEngine.js
@@ -1,26 +1,12 @@
 class routesEngine{
     routeIndex=window.location.href
     routeVars = []
-    routePath = []
     listenersEvents = [0]
     //Define Routes
     goToLink(link){
-        let verifyRoute=false
-        //Verify if route is exist in context
-        this.routePath.forEach(element => {
-            if(link.includes(element)){
-                console.log("rota verificada!")
-                verifyRoute=true
-            }
-        });
-        if(verifyRoute==true){
-            history.pushState(link,link)
-            this.routeIndex=link
-            window.location.href=link
-        }
-        else{
-            document.getElementById("app").innerText="404 route dont found"
-        }
+        history.pushState(link,link)
+        this.routeIndex=link
+        window.location.href=link
     }
     //Get vars Routes for pass for own vars
     getRouteVars(){
@@ -39,9 +25,6 @@ class routesEngine{
            }
         }
         this.routeVars.push(varB)
-    }
-    registerRoute(routePathRegister){
-        this.routePath.push(routePathRegister)
     }
     //When executes update event in page
     whenUpdate(eventClass){
