cae19cac7dc7692f27ad5adb8fbd89bcc6a3f5c3
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-07-21 21:31:49 -0300

    render options ok

--- Framework/render.js
+++ Framework/render.js
@@ -1,10 +1,34 @@
 class RenderEngine{
+    renderElementInPage=[]
+    registerPage =[]
     //Render object page
     renderPage(renderElement,content,selectObjct){
+        let render = false
         let renderCreate = document.createElement(renderElement)
         let nodeContent = document.createTextNode(content)
-        renderCreate.appendChild(nodeContent)
-        document.getElementById(selectObjct).appendChild(renderCreate)
+        let register = 0
+        this.renderElementInPage.forEach(element => {
+            if(document.getElementById(element).id == document.getElementById(selectObjct).id){
+                render = true
+            }
+        })
+        while(this.renderElementInPage.length>register){
+            if(document.getElementById(this.renderElementInPage[register]).id == document.getElementById(selectObjct).id){
+                break
+            }
+            register+=1
+        }
+        if(render == false){
+            register = parseInt(Math.random()*10)+"id"+parseInt(Math.random()*4)
+            renderCreate.id = register
+            renderCreate.appendChild(nodeContent)
+            document.getElementById(selectObjct).appendChild(renderCreate)
+            this.renderElementInPage.push(selectObjct)
+            this.registerPage.push(register)
+        }
+        else{
+            document.getElementById(this.registerPage[register]).innerHTML = content
+        }
     }
 }
 
