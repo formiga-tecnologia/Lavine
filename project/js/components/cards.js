10bb45130b8678a81429644754e41badf47a6ba6
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-08-08 22:07:15 -0300

    update index functional API

--- project/js/components/cards.js
+++ project/js/components/cards.js
@@ -1,17 +1,17 @@
 import EngineApp from '../../../Framework/engine.js'
-
 //render obj
 var compRender = new EngineApp()
-var comp = [
-    "<div id='card0'>"+"<br>"+
-    "<div id='header0' class='card-header'></div>"+
-    "<div id='card1' class='card-body'>"+
-    "<p id='contentBody'></p>"+
-    "<br> <button id='clBt'>Consultar Dados</button> "
-    +"</div"
-]
+
 //renderização de objeto tipo 'card'
 function cards(){
+    var comp = [
+        "<div id='card0'>"+"<br>"+
+        "<div id='header0' class='card-header'></div>"+
+        "<div id='card1' class='card-body'>"+
+        "<p id='contentBody'>"+"</p>"+
+        "<br> <button id='clBt'>Consultar Dados</button> "
+        +"</div"
+    ]
     compRender.componentsEngine.createComponent("card",comp)
     compRender.componentsEngine.renderComponent("dinamicType","card")
 }
--- project/js/index.js
+++ project/js/index.js
@@ -12,19 +12,30 @@ menu() //Render Menu
 //Add rota
 eng.routesEngine.registerRoute("http://127.0.0.1:5500/project/","initialPage")
 eng.routesEngine.registerRoute("http://127.0.0.1:5500/project/index.html#pageInitial",welcomeguide)
-eng.routesEngine.registerRoute("http://127.0.0.1:5500/project/index.html#pages/docs",cards)
+eng.routesEngine.registerRoute("http://127.0.0.1:5500/project/index.html#pages/docs",docsPage)
+eng.routesEngine.registerRoute("http://127.0.0.1:5500/project/index.html#pages/docs/erro",error)
 
 eng.routesEngine.runRoute("http://127.0.0.1:5500/project/index.html","http://127.0.0.1:5500/project/index.html#pageInitial")
 
 
+function docsPage(){
+    if(eng.routesEngine.routePropsVars.length!=1){
+        eng.routesEngine.goToLink("http://127.0.0.1:5500/project/index.html#pages/docs/erro")
+    }
+    else{
+        cards()
+        Api(eng.routesEngine.routePropsVars[0])
+    }
+}
+
 
 //API
-//function Api(data) {  
-    //fetch('https://jsonplaceholder.typicode.com/posts/' + data)
-      //  .then(response => response.json())
-      //  .then(json => eng.renderEngine.changeContentElement("header0", json.title))
+function Api(data) {  
+    fetch('https://jsonplaceholder.typicode.com/posts/' + data)
+        .then(response => response.json())
+        .then(json => eng.renderEngine.changeContentElement("header0", json.title))
 
-    //fetch('https://jsonplaceholder.typicode.com/posts/' + data)
-      //  .then(response => response.json())
-       // .then(json => eng.renderEngine.changeContentElement("contentBody", json.body))
-//}
+    fetch('https://jsonplaceholder.typicode.com/posts/' + data)
+        .then(response => response.json())
+        .then(json => eng.renderEngine.changeContentElement("contentBody", json.body))
+}
