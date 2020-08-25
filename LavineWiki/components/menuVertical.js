ece08543852de44c97578b53086fd0218f3adc62
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-08-24 23:59:12 -0300

    component menu

--- LavineWiki/components/menuVertical.js
+++ LavineWiki/components/menuVertical.js
@@ -3,15 +3,17 @@ let eng =new EngineApp()
 
 
 function menuVertical(){
-
+    eng.componentsEngine.createComponent("vertical",menuVerticalRender())
+    eng.componentsEngine.renderComponent("dinamic","vertical")
 }
 
 function  menuVerticalRender(){
-    return "<ul>"+
-    "<li class='nav-item'>"+
-    ""+
-    ""+
-    "</ul>"
+    return "<div class='vertical-nav bg-white'>"+
+    "<div class='py-4 px-3 mb-4 bg-light'>"+
+    "<div class='media-body'><h4 class='m-0'>Lavine</h4>"+
+    "<p class='font-weight-light text-muted mb-0>Front end Framework</p>"+
+    "</div>"+
+    "</div></div>"
 }
 
 
--- LavineWiki/pages/docs.js
+++ LavineWiki/pages/docs.js
@@ -1,10 +1,12 @@
 import EngineApp from '../../Framework/engine.js'
 import blackbox from '../components/blackBox.js'
+import menuVertical from '../components/menuVertical.js'
 
 let eng = new EngineApp()
 
 function docs(){
     let eng = new EngineApp()
+    menuVertical()
     blackbox("Lavine Testes","que isso rapaz a lavine é braba!")
 }
 
