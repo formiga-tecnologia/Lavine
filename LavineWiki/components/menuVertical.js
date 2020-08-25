e6feb1c5d4644badafb60e4764158b5e0592b89f
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-08-24 23:32:58 -0300

    create menu vertical component test

---
+++ LavineWiki/components/menuVertical.js
@@ -0,0 +1,19 @@
+import EngineApp from '../../Framework/engine.js'
+let eng =new EngineApp()
+
+
+function menuVertical(){
+
+}
+
+function  menuVerticalRender(){
+    return "<ul>"+
+    "<li class='nav-item'>"+
+    ""+
+    ""+
+    "</ul>"
+}
+
+
+
+export default menuVertical
