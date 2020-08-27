c2ca3276fc26435b7fc0dbc118e5fa047f99897d
Felipe Catão do Nascimento <34559081+FelipeKatao@users.noreply.github.com>
2020-08-27 09:48:38 -0300

    Update docs.js

--- LavineWiki/pages/docs.js
+++ LavineWiki/pages/docs.js
@@ -16,7 +16,8 @@ function Renderdocs(){
     "<ul> <li>Aprenda Lavine</li> <li>Estrutura basica</li> <li>Começando um projeto</li> <li>Construção do projeto</li> <li>Passos Avançados</li> </ul>")
     article(1,"Principais conceitos","Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quae quasi tenetur, commodi voluptas aperiam sunt alias autem molestiae sit exercitationem! Labore quo optio placeat minima vero id quis! Dicta.")
     article(2,"Pratica inicial","Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, eaque? Tempore sint laboriosam tenetur. Ullam alias, corporis aperiam aspernatur esse quod nam. Officia consequuntur laborum eligendi dolorem, debitis temporibus. Id? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, quasi a! Commodi ipsam assumenda, corrupti mollitia nisi optio doloremque ratione velit quo illum nam. Aut quibusdam fugit perferendis labore quam?")
+    article(3,"Sobre funções","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum")
 }
 
-
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
 export default docs
