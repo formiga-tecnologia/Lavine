45f5cebe211b9f3a9fa3dbe6a85c4e39600ee57b
Felipe Catão do Nascimento <34559081+FelipeKatao@users.noreply.github.com>
2020-08-27 09:41:54 -0300

    add card people

--- LavineWiki/pages/about.js
+++ LavineWiki/pages/about.js
@@ -7,6 +7,7 @@ function AboutPage(){
     eng.componentsEngine.renderComponent("dinamic-Article","article")
     cardPeople(0,"team","https://avatars2.githubusercontent.com/u/34559081?s=460&u=cb0712157640e18dea3c4bb479c861c59a28ef97&v=4","Felipe Katao","Developer & CTO","Felipe Katao is developer and design of core the Lavine.js and other products of Formiga Techs")
     cardPeople(1,"team","https://avatars2.githubusercontent.com/u/62511114?s=460&u=2139e5db06ecab070ab08247af1bba3b52161791&v=4","Marcia Alves","UX & QA process","Marcia construct one interface for products and services,and control t5he process of quality the products in Formiga Tech]")
+    cardPeople(2,"team","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTRDof7bpR3WN6nyh5xsNOpwIZVskJBIBd1LA&usqp=CAU","Patrick Silva","Designer","Patrick created sceens page for lavine.")
 }
 function AboutRender(){
     return "<br><br><article class='articleComp container'><h1>About our team!</h1>"+
