2d8b7df83aa289c0a7c9402118a3d73af5d429f7
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-08-29 00:55:17 -0300

    update class

--- LavineWiki/pages/home.js
+++ LavineWiki/pages/home.js
@@ -16,11 +16,11 @@ function goFunctions(){
    rot.doc()
 }
 function articlePage(){
-    return "<br> <br><br><div id='articles' class='container'>"+
+    return "<br> <br><br><div id='articles' class='articleComp container'>"+
     "<div class='row'>"+
     " <div class='col-md-8'><h1>What is Lavine</h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt consectetur odio praesentium? Omnis, deleniti? Necessitatibus, alias doloremque tempore velit dolorem incidunt est atque ab corrupti reiciendis ratione sequi impedit. Dolores?</div>"+
     "<div class='col-6 col-md-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed velit nisi iure officia voluptas? Impedit suscipit veritatis ratione et sint quos necessitatibus illo cum provident! Voluptas ex necessitatibus aperiam reiciendis?</div>"+
     "</div>"
 }
-
+ 
 export default homePage
