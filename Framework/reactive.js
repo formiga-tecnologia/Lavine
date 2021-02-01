0507f82fe5acee159bdfdd3ea4fc7a2230706cca
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2021-02-01 20:48:10 -0300

    create reactive class

---
+++ Framework/reactive.js
@@ -0,0 +1,17 @@
+/*This is a reactive class design component, your responsability is create one 
+lists events where our events executable in order.
+*/
+
+class Reactive{
+
+    ReactiveList = []
+    ReactiveList_status = []
+    ReactiveList_notify = []
+
+    createCycle(){
+        
+    }
+
+}
+
+export default Reactive
