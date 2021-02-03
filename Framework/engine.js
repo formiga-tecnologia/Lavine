c95c074f8d92b3ea599206b10c5d5faa7db2e52b
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2021-02-02 21:30:24 -0300

    update engine

--- Framework/engine.js
+++ Framework/engine.js
@@ -2,12 +2,14 @@ import routesEngine from './routesEngine.js'
 import RenderEngine from './render.js'
 import Components from './components.js'
 import Security from './security.js'
+import reactive from './reactive.js'
 class EngineApp{
     count=0
     routesEngine = new routesEngine
     renderEngine = new RenderEngine
     componentsEngine = new Components
     Security = new Security
+    Reactive = new reactive
 }
 
 export default  EngineApp
